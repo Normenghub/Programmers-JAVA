7f3d94718bf39f5d5cd22fe9f998944b0df08418
SHEEN <normengdie@pusan.ac.kr>
2024-08-17 22:31:14 +0900

    docs : create 서울에서김서방찾기.java

---
+++ 서울에서김서방찾기.java
@@ -0,0 +1,15 @@
+public class 서울에서김서방찾기 {
+    class Solution {
+        public String solution(String[] seoul) {
+            String answer = "";
+            int i;
+            for(i = 0; i<seoul.length; i++){
+                if(seoul[i].equals("Kim")){
+                    break;
+                }
+            }
+            answer = "김서방은 " +i +"에 있다" ;
+            return answer;
+        }
+    }
+}
