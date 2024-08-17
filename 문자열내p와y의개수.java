392233de5b9f88cdb6996880fea81ae5bb874d9c
SHEEN <normengdie@pusan.ac.kr>
2024-08-17 20:54:38 +0900

    docs : create 문자열내p와y의개수.java

---
+++ 문자열내p와y의개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        int p,y;
+        p = 0;
+        y = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(Character.toLowerCase(s.charAt(i)) == 'p'){
+                p++;
+            }else if(Character.toLowerCase(s.charAt(i)) == 'y'){
+                y++;
+            }
+        }
+        if(p == y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
