420648d5662ce945f60361a72fbe6dd0574ed0a1
SHEEN <normengdie@pusan.ac.kr>
2024-08-19 13:33:14 +0900

    docs : create 음양더하기.java

---
+++ 음양더하기.java
@@ -0,0 +1,15 @@
+public class 음양더하기 {
+    class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+            for(int i = 0; i < signs.length; i++){
+                if(signs[i] == false){
+                    answer -= absolutes[i];
+                }else{
+                    answer+=absolutes[i];
+                }
+            }
+            return answer;
+        }
+    }
+}
