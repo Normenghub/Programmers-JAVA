3b0213cc06c3de11d5d87be16c9fecfef2ad6b18
SHEEN <normengdie@pusan.ac.kr>
2024-08-26 13:23:13 +0900

    docs : create 명예의 전당 (1).java

---
+++ 명예의전당1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class 명예의전당1 {
+    class Solution {
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+
+            ArrayList<Integer> list = new ArrayList();
+
+            for(int i=0; i<score.length; i++){
+                list.add(score[i]);
+                Collections.sort(list, Collections.reverseOrder());
+                if(list.size() < k){
+                    answer[i] = list.get(list.size()-1);
+                }else{
+                    answer[i] = list.get(k-1);
+                }
+            }
+            return answer;
+        }
+    }
+}
