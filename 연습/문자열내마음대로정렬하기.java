38b154275b616c771aeafaea2a41af3004d53ae3
SHEEN <normengdie@pusan.ac.kr>
2024-10-03 04:19:02 +0900

    feat : create 문자열 내 마음대로 정렬하기.java

---
+++ 연습/문자열내마음대로정렬하기.java
@@ -0,0 +1,18 @@
+package 연습;
+import java.util.*;
+public class 문자열내마음대로정렬하기 {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        ArrayList<String> list = new ArrayList();
+        for(int i = 0; i<strings.length; i++){
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(list);
+        for(int i = 0; i<answer.length; i++){
+            answer[i] = list.get(i).substring(1,list.get(i).length());
+        }
+        return answer;
+    }
+    class Solution {
+    }
+}
