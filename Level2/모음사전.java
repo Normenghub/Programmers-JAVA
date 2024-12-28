e44f9dcd1daee3897958f038d51a9ae09167a23d
SHEEN <sheennho@gmail.com>
2024-12-28 19:06:23 +0900

    create : 모음 사전.java

---
+++ Level2/모음사전.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    static ArrayList<String> strList = new ArrayList<>();
+    static String[] words = {"A", "E", "I", "O", "U"};
+    public int solution(String word) {
+        for (String str : words) {
+            depth(str);
+        }
+        return strList.indexOf(word) + 1;
+    }
+    public static void depth(String word) {
+        if(word.length() <= 5){
+            strList.add(word);
+            for(String data : words){
+                depth(word  + data);
+            }
+        }
+
+    }
+}
